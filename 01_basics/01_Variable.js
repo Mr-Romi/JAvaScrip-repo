04adb1fe42179c2c1978b22cb820dd47af3fff6a
Romind More <82325817+Mr-Romi@users.noreply.github.com>
2026-07-01 02:13:46 +0000

    Data type

---
+++ 01_basics/01_Variable.js
@@ -0,0 +1,11 @@
+let accountEmail = "romidaa@gmail.com"
+var accountPassword = "12345"
+const accountId = "5446643"
+
+console.log();
+console.table([accountId , accountEmail , accountPassword])
+
+/*
+Dont use Var 
+issue in block scope
+*/
---
+++ 01_basics/02_Datatype.js
@@ -0,0 +1,6 @@
+/* 
+    number ==> 2 , 3  , 2 to power 33
+    bigint
+    String ==> ""
+    boolean 
+*/
