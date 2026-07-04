9569330e2b0e25204e6bcd37fea08541a62c7add
Romind More <82325817+Mr-Romi@users.noreply.github.com>
2026-07-04 01:01:32 +0000

    Conversion

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -1,4 +1,27 @@
-let score = "38"
+let score = "38anc"
 
-console.log(typeof score);
-console.log(typeof(score));
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+/**
+ string
+string
+number
+NaN
+ */
+
+//"33" ==> 33
+//
+
+l
+
+
+
+
+
+/* ***********************Operations*********** */ 
+
